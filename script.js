0d27b6fb17411cbda4f8167feacb20ad7a3989cf
jbowen999 <julian.e.bowen@outlook.com>
2024-06-13 14:32:12 -0700

    added local host sotrage

--- script.js
+++ script.js
@@ -2,11 +2,28 @@ const userInput = document.getElementById("todo-input");
 const form = document.getElementById("todo-form");
 const list = document.getElementById("todo-list");
 
+window.addEventListener("DOMContentLoaded", function (event) {
+  const items = JSON.parse(localStorage.getItem("myTodoItems"));
+  items.map((item) => makeTodo(item));
+});
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   const inputValue = userInput.value;
+  makeTodo(inputValue);
+  const items = JSON.parse(localStorage.getItem("myTodoItems"));
+  if (items) {
+    items.push(inputValue);
+    localStorage.setItem("myTodoItems", JSON.stringify(items));
+  } else {
+    localStorage.setItem("myTodoItems", JSON.stringify([inputValue]));
+  }
+  userInput.value = "";
+});
+
+const makeTodo = function (str) {
   const listItem = document.createElement("li");
-  const listText = document.createTextNode(inputValue);
+  const listText = document.createTextNode(str);
   listItem.appendChild(listText);
 
   const removeBtn = document.createElement("button");
@@ -16,9 +33,7 @@ form.addEventListener("submit", function (event) {
 
   list.appendChild(listItem);
 
-  userInput.value = "";
-
   removeBtn.addEventListener("click", function () {
     list.removeChild(listItem);
   });
-});
+};
